e857730ad4f81de76e7b1d639b5e36627737e67b
Marco Climent <cem201409@gmail.com>
2021-01-14 19:11:33 +0100

    no message

--- src/App.js
+++ src/App.js
@@ -1,52 +1,11 @@
 import './App.css';
 import * as React from 'react';
 import { Route, Switch, NavLink } from 'react-router-dom';
-import {Component} from 'react';
-//import { ComponentName } from 'primereact/{componentname}';
-import { InputText } from 'primereact/inputtext';
-import { Button } from 'primereact/button';
+import ApuestasView from './ApuestasView';
+import EventosView from './EventosView';
+import UsuariosView from './UsuariosView';
+import InformesView from './InformesView';
 
-
-
-class UsuariosView extends Component{
-  render() {
-    const [valor, setValor] = '';
-    return (
-      <div class="row">
-        <div class="column left">
-        <Button icon="pi pi-plus" iconPos="right" />
-        </div>
-        <div class="column right">
-        <Button icon="pi pi-minus" iconPos="right" />
-        </div>
-        <div class="column middle">
-          <span className="p-float-label">
-          <InputText id="username" valor={valor} onChange={(e) => 
-          setValor(e.target.value)} />
-          </span>
-          <label htmlFor="username">Filtrar...</label></div>
-      </div>
-    );
-  }
-}
-
-class ApuestasView extends Component{
-  render() {
-    return <h1>This is the Apuestas view</h1>;
-  }
-}
-
-class EventosView extends Component{
-  render() {
-    return <h1>This is the Eventos view</h1>;
-  }
-}
-
-class InformesView extends Component{
-  render() {
-    return <h1>This is the Informes view</h1>;
-  }
-}
 
 function App() {
   return (
