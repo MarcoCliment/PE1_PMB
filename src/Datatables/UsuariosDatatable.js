e0c97dfb0c05b3157056874ab44f9e6c7c4fd2b7
Marco Climent <cem201409@gmail.com>
2021-01-16 18:32:00 +0100

    Terminada vista usuarios, implementación de eliminado y restablecimiento del password

--- src/Datatables/UsuariosDatatable.js
+++ src/Datatables/UsuariosDatatable.js
@@ -5,27 +5,64 @@ import 'primeflex/primeflex.css';
 import '../App.css';
 import ReactDOM from 'react-dom';
 
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { DataTable, Column } from 'primereact/datatable';
+import { ConfirmDialog, confirmDialog } from 'primereact/confirmdialog'; // To use confirmDialog method
 import { Card } from 'primereact/card';
 import { Button } from 'primereact/button';
+import { Toast } from 'primereact/toast';
+
 import Datos from '../UsuariosV';
 
 const UsuariosDatatable = () => {
     const [usuarios, setUsuarios] = useState(null);
     const usuariosGet = new Datos();
+    const toast = useRef(null);
 
-    const footer = (
-        <div>
-            <Button label="Añadir" icon="pi pi-plus" />
-            <Button label="Eliminar" icon="pi pi-minus" className="p-button-secondary p-ml-2" />
-        </div>   
-    );
+    const confirmPw = () => {
+        confirmDialog({
+            message: 'Se restablecerá la contraseña, ¿está seguro?',
+            header: 'Confirmación',
+            icon: 'pi pi-exclamation-triangle',
+            accept: acceptPw,
+            reject: reject
+        });
+    }
+    
+    var currRowData = null
+    const confirmDel = (rowData) => {
+        currRowData = rowData
+        confirmDialog({
+            message: 'Se eliminará el usuario ' + rowData.nombre + ' ' + rowData.apellidos +  ', ¿está seguro?',
+            header: 'Confirmación',
+            icon: 'pi pi-exclamation-triangle',
+            accept: acceptDel,
+            reject: reject
+        });
+    }
+
+    const acceptPw = () => {
+        toast.current.show({ severity: 'info', summary: 'Confirmado', detail: 'Se ha restablecido la contraseña', life: 3000 });
+        //TODO - EN NUESTRA BASE DE DATOS NO EXISTE EL CAMPO CONTRASEÑA, PARA IMPLEMENTARLO, LANZARÍAMOS UN PUT DE LA CONTRASEÑA A VACÍO PARA FORZAR AL USUARIO A ASIGNARLA CUANDO VOLVIESE A INICIAR SESIÓN
+    }
+    const acceptDel = () => {
+        toast.current.show({ severity: 'info', summary: 'Confirmado', detail: 'Se ha eliminado el usuario ' + currRowData.nombre + ' ' + currRowData.apellidos +  '', life: 3000 });
+        var uv = new Datos();
+        uv.deleteUsuarios(currRowData.email)
+        setTimeout(()=>{
+            usuariosGet.getUsuarios().then(res => setUsuarios(res));
+        },500)
+    }
+
+    const reject = () => {
+        toast.current.show({ severity: 'info', summary: 'Cancelado', detail: 'Operación cancelada', life: 3000 });
+    }
+
     const herramientas = (rowData) => {
         return (
             <React.Fragment>
-                <Button icon="pi pi-unlock" className="p-button-rounded p-button-success p-mr-2" />
-                <Button icon="pi pi-trash" className="p-button-rounded p-button-warning" />
+                <Button icon="pi pi-unlock" onClick={confirmPw} className="p-button-rounded p-button-success p-mr-2" />
+                <Button icon="pi pi-trash" onClick={() => confirmDel(rowData)} className="p-button-rounded p-button-warning" />
             </React.Fragment>
         );
     }
@@ -35,19 +72,22 @@ const UsuariosDatatable = () => {
     }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
     return (
-        <div>
-            <DataTable value={usuarios} header="Lista de usuarios"
-            className="p-datatable-gridlines">                    
-                <Column field="nombre" header="Nombre" filter filterPlaceholder="Filtrar por nombre" ></Column>
-                <Column field="apellidos" header="Apellidos" filter filterPlaceholder="Filtrar por apellidos" ></Column>
-                <Column field="email" header="Email" filter filterPlaceholder="Filtrar por email"></Column>
-                <Column field="banco" header="Banco"></Column>
-                <Column body={herramientas}></Column>
-            </DataTable>
+        <div>            
+            <Toast ref={toast} />
+            <Card>
+                <DataTable value={usuarios} header="Lista de usuarios"
+                    className="p-datatable-gridlines">
+                    <Column field="nombre" header="Nombre" filter filterPlaceholder="Filtrar por nombre" ></Column>
+                    <Column field="apellidos" header="Apellidos" filter filterPlaceholder="Filtrar por apellidos" ></Column>
+                    <Column field="email" header="Email" filter filterPlaceholder="Filtrar por email"></Column>
+                    <Column field="banco" header="Banco"></Column>
+                    <Column body={herramientas}></Column>
+                </DataTable>
+            </Card>
         </div>
     );
 }
-                
+
 const rootElement = document.getElementById("root");
 ReactDOM.render(<UsuariosDatatable />, rootElement);
 
--- src/UsuariosV.js
+++ src/UsuariosV.js
@@ -5,6 +5,9 @@ export default class Datos {
     getUsuarios() {
         return axios.get('http://localhost:54446/api/usuarios').then(res => res.data);
     }
+    deleteUsuarios(email){
+        return axios.delete('http://localhost:54446/api/usuarios/' + email)
+    }
     getEventos() {
         return axios.get('http://localhost:54446/api/eventos').then(res => res.data);
     }
